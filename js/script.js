d764117d61afce2bc916eb5cb3ff6ed743ba8c3d
Kate Pronina <kate-pronina-97@yandex.ru>
2017-08-15 01:05:26 +0300

    Переписала меню на jquery

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
-var popupMenu = document.querySelector(".mobile-menu");
-var openButton = document.querySelector(".header__menu-adaptive");
-var closeButton = document.querySelector(".mobile-menu__btn");
+$(document).ready(function() {
 
-openButton.addEventListener("click", function (event) {
-	event.preventDefault();
-	popupMenu.classList.add("mobile-menu_open");
+	var popupMenu = $('.mobile-menu');
+	var openButton = $('.header__menu-adaptive');
+	var closeButton = $('.mobile-menu__btn');
+
+	openButton.on('click', function(e) {
+		e.preventDefault();
+
+		popupMenu.fadeIn(300);
+		$('.header__logo').css('position', 'fixed');
+	});
+
+	closeButton.on('click', function(e) {
+		e.preventDefault();
+
+		popupMenu.fadeOut(300);
+		$('.header__logo').css('position', 'static');
+	});
+
 });
 
-closeButton.addEventListener( "click", function (event) {
-	event.preventDefault();
-	popupMenu.classList.remove("mobile-menu_open");
-});
+
+
+
+// var popupMenu = document.querySelector(".mobile-menu");
+// var openButton = document.querySelector(".header__menu-adaptive");
+// var closeButton = document.querySelector(".mobile-menu__btn");
+
+// openButton.addEventListener("click", function (event) {
+// 	event.preventDefault();
+// 	popupMenu.classList.add("mobile-menu_open");
+// });
+
+// closeButton.addEventListener( "click", function (event) {
+// 	event.preventDefault();
+// 	popupMenu.classList.remove("mobile-menu_open");
+// });
